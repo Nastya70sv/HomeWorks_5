57d146a4a49be0965bc11a88096cdba2094910e1
Anastasia <nastya70sv@mail.ru>
2022-12-01 23:43:40 +0300

    Added 36

--- HW_36/Program.cs
+++ HW_36/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях(индексах).
+int[] FillArray(int size)
+{
+int[] arr = new int[size];
+for(int i = 0; i < arr.Length; i++)
+{
+arr[i] = new Random().Next(10,1000);
+}
+return arr;
+}
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine("Массив : " +string.Join(", ", array));
+int sum = 0;
+
+for (int i = 1; i < array.Length; i+=2)
+    sum = sum + array[i];
+
+    Console.WriteLine($"всего {array.Length} чисел, сумма элементов cтоящих на нечётных позициях = {sum}");
+
+
